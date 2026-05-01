f24b1ebdab4f5d964be9179a4d76235f63d617f8
blrmehdi00-ship-it <blrmehdi00@gmail.com>
2026-05-02 00:46:24 +0100

    Update sim.launch.py

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -1,22 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
 
+    urdf_path = os.path.join(
+        get_package_share_directory('my_robot_project'),
+        'urdf',
+        'robot.urdf'
+    )
+
     return LaunchDescription([
 
         ExecuteProcess(
-            cmd=['gazebo', '--verbose'],
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
             output='screen'
         ),
 
         Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
-            arguments=['-entity', 'my_robot', '-file',
-                       os.path.join(os.getcwd(), 'src/my_robot_project/urdf/robot.urdf')]
+            arguments=['-entity', 'my_robot', '-file', urdf_path]
         ),
 
         Node(
